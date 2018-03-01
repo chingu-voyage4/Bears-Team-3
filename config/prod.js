366e3165d7a29d5dd2ceabccd75646e8212a096b
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-01 09:38:59 -0800

    Delete unused files

--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  githubClientID: process.env.GITHUB_CLIENT_ID,
-  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-};
