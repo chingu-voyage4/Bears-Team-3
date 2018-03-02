295ce3458a532a775ae6ed0e551d03ac5c28a3ac
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-01 22:07:23 -0800

    Remove extra require statement

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ require('./routes/authRoutes')(app);
 // Handle client routes in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
