c2eb2c81a8e4b8f9092af793927982e770ac6c08
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-06 01:43:01 -0800

    Remove MuiThemeProvider from Home

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 
 import * as actions from '../actions';
 
 const Home = ({ counterValue, handleIncreaseValue, handleDecreaseValue }) => (
-  <MuiThemeProvider>
-    <div>
-      <h2>Home Page</h2>
-      <RaisedButton label="Default" />
-      <p>The counter value is {counterValue}</p>
-      <button onClick={handleIncreaseValue}>Add</button>
-      <button onClick={handleDecreaseValue}>Remove</button>
-    </div>
-  </MuiThemeProvider>
+  <div>
+    <h2>Home Page</h2>
+    <RaisedButton label="Default" />
+    <p>The counter value is {counterValue}</p>
+    <button onClick={handleIncreaseValue}>Add</button>
+    <button onClick={handleDecreaseValue}>Remove</button>
+  </div>
 );
 
 Home.propTypes = {
