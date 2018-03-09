d3a60ef52650b8fd9cd0726afca8478ead9f4b51
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-09 10:10:56 -0800

    Redirect authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,11 +3,17 @@ const passport = require('passport');
 module.exports = app => {
   app.get('/auth/github', passport.authenticate('github'));
 
-  app.get('/auth/github/callback', passport.authenticate('github'));
+  app.get(
+    '/auth/github/callback',
+    passport.authenticate('github'),
+    (req, res) => {
+      res.redirect('/');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
