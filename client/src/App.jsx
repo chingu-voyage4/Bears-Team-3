2256c0f4e0628f4ca80aa8a93c6cefda4dfc4939
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-09 09:43:40 -0800

    Connect App.jsx & fetchUser

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Link,
-} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+import * as actions from './actions';
 import Routes from './Routes';
 
 import reactLogo from './assets/React-icon.png';
@@ -12,21 +11,37 @@ import reactLogo from './assets/React-icon.png';
  * this container is defined as class so we can modify state
  * @return {Component} react base component
  */
-const App = () => (
-  <BrowserRouter>
-    <main className="container">
-      <div>
-        <h1>hello world!</h1>
-        <img className="container__image" alt="react logo" src={reactLogo} />
-        <p>If you see this everything is working!</p>
-      </div>
-      <ul className="left">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-      <Routes />
-    </main>
-  </BrowserRouter>
-);
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <BrowserRouter>
+        <main className="container">
+          <div>
+            <h1>hello world!</h1>
+            <img
+              className="container__image"
+              alt="react logo"
+              src={reactLogo}
+            />
+            <p>If you see this everything is working!</p>
+          </div>
+          <ul className="left">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+          <Routes />
+        </main>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
