f0494620e983031f59754d41eab870211b481b67
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-11 16:17:19 -0700

    Add Origin, X-Requested-With, Content-Type, Accept

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ app.use(passport.session());
 
 app.all('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
