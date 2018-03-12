f6791d526813eb011099d52be009ec62f0ebce41
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-12 01:09:08 -0700

    Add more headers to app.all

--- index.js
+++ index.js
@@ -39,9 +39,11 @@ app.use(passport.session());
 app.all('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+
   next();
 });
 
