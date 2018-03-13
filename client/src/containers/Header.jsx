51ca1e0656ed45caaab1e9326bdc895b0a11d1cf
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-13 12:00:59 -0700

    Get App Bar example code working

--- client/src/containers/Header.jsx
+++ client/src/containers/Header.jsx
@@ -1,50 +1,170 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
 
-import * as actions from '../actions';
+// import * as actions from '../actions';
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
+// class Header extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.handleLogin = this.handleLogin.bind(this);
-  }
+//     this.handleLogin = this.handleLogin.bind(this);
+//   }
 
-  handleLogin() {
-    // Workaround to avoid re-rendering & CORS issues Credit @jenovs https://github.com/jenovs & https://stackoverflow.com/questions/28392393/passport-js-after-authentication-in-popup-window-close-it-and-redirect-the-pa/29314111#29314111
-    window.open('/auth/github', '_blank', 'width=300,height=400');
-  }
+//   handleLogin() {
+//     // Workaround to avoid re-rendering & CORS issues Credit @jenovs https://github.com/jenovs & https://stackoverflow.com/questions/28392393/passport-js-after-authentication-in-popup-window-close-it-and-redirect-the-pa/29314111#29314111
+//     window.open('/auth/github', '_blank', 'width=300,height=400');
+//   }
 
-  renderContent() {
-    switch (this.props.auth) {
-      case null:
-        return;
-      case false:
-        return (
-          <button
-            type="button"
-            className="nav__login"
-            onClick={this.handleLogin}
-          >
-            Log In With Github
-          </button>
-        );
-      default:
-        return (
-          <div className="nav__logout">
-            <a href="/api/logout">Logout</a>
-          </div>
-        );
-    }
-  }
+//   renderContent() {
+//     switch (this.props.auth) {
+//       case null:
+//         return;
+//       case false:
+//         return (
+//           <button
+//             type="button"
+//             className="nav__login"
+//             onClick={this.handleLogin}
+//           >
+//             Log In With Github
+//           </button>
+//         );
+//       default:
+//         return (
+//           <div className="nav__logout">
+//             <a href="/api/logout">Logout</a>
+//           </div>
+//         );
+//     }
+//   }
+
+//   render() {
+//     return <nav className="nav">{this.renderContent()}</nav>;
+//   }
+// }
+
+// const mapStateToProps = state => ({
+//   auth: state.authReducer,
+// });
+
+// export default connect(mapStateToProps, actions)(Header);
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import Switch from 'material-ui/Switch';
+import { FormControlLabel, FormGroup } from 'material-ui/Form';
+import Menu, { MenuItem } from 'material-ui/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class MenuAppBar extends React.Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleChange = (event, checked) => {
+    this.setState({ auth: checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
 
   render() {
-    return <nav className="nav">{this.renderContent()}</nav>;
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={auth}
+                onChange={this.handleChange}
+                aria-label="LoginSwitch"
+              />
+            }
+            label={auth ? 'Logout' : 'Login'}
+          />
+        </FormGroup>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="title"
+              color="inherit"
+              className={classes.flex}
+            >
+              Title
+            </Typography>
+            {auth && (
+              <div>
+                <IconButton
+                  aria-owns={open ? 'menu-appbar' : null}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = state => ({
-  auth: state.authReducer,
-});
+MenuAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-export default connect(mapStateToProps, actions)(Header);
+export default withStyles(styles)(MenuAppBar);
