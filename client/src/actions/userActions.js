4065d217f06b39f88a97418ed0235bd312c605e8
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-14 12:43:15 -0700

    Remove res.data from logout action

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 import { LOGOUT, FETCH_USER } from './types';
 
 export const logout = () => async dispatch => {
-  const res = await axios.get('/api/logout');
-  const resData = res.data;
-  dispatch({ type: LOGOUT, resData });
+  await axios.get('/api/logout');
+  dispatch({ type: LOGOUT });
 };
 
 export const fetchUser = () => async dispatch => {
