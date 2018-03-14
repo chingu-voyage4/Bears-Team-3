86783fad767890bf0ed418858a4f762176bb2cda
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-14 15:26:25 -0700

    Add book icon

--- client/src/containers/Header.jsx
+++ client/src/containers/Header.jsx
@@ -9,6 +9,7 @@ import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import Avatar from 'material-ui/Avatar';
+import BookIcon from 'material-ui-icons/ImportContacts';
 import Menu, { MenuItem } from 'material-ui/Menu';
 
 import * as actions from '../actions';
@@ -60,6 +61,13 @@ class Header extends Component {
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <BookIcon />
+            </IconButton>
             <Typography
               variant="title"
               color="inherit"
