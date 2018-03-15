80c56b0cea2fe0be59de1df95b40d1c660711dc9
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-15 15:14:05 -0700

    Snapshot test Header, failed proptype

--- client/src/tests/__tests__/Header.test.js
+++ client/src/tests/__tests__/Header.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Header } from './../../containers';
+import { Header } from './../../containers/Header';
 import MockProvider from '../mockProvider';
 
 it('renders without crashing', () => {
   const snap = shallow(
     <MockProvider>
-      <Header />
+      <Header classes={true} />
     </MockProvider>
   );
   expect(snap).toMatchSnapshot();
