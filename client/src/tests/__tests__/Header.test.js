ce174d36b7fff9d0b8722d4fc2e9c2a4e17e9fcf
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-15 15:42:47 -0700

    Header test working

--- client/src/tests/__tests__/Header.test.js
+++ client/src/tests/__tests__/Header.test.js
@@ -4,9 +4,22 @@ import { Header } from './../../containers/Header';
 import MockProvider from '../mockProvider';
 
 it('renders without crashing', () => {
+  const classes = {
+    root: {
+      flexGrow: 1,
+      textAlign: 'left',
+    },
+    flex: {
+      flex: 1,
+    },
+    avatar: {
+      margin: 10,
+    },
+  };
+
   const snap = shallow(
     <MockProvider>
-      <Header classes={true} />
+      <Header classes={classes} />
     </MockProvider>
   );
   expect(snap).toMatchSnapshot();
