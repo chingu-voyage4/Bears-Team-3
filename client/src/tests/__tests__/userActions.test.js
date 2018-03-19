165ff87ca2c1c930fff0b451ce704cae97a06f47
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-19 09:30:42 -0700

    Thunk tests passing

--- client/src/tests/__tests__/userActions.test.js
+++ client/src/tests/__tests__/userActions.test.js
@@ -4,6 +4,7 @@ import moxios from 'moxios';
 
 import { logout, fetchUser } from '../../actions/userActions';
 import { LOGOUT, FETCH_USER } from '../../actions/types';
+import fetchUserMock from '../__mock__/fetchUserMock.json';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -34,56 +35,19 @@ describe('User actions', () => {
   });
 
   it('should handle FETCH_USER action', () => {
-    const store = mockStore({});
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: fetchUserMock,
+      });
+    });
+
+    const store = mockStore({ resData: {} });
 
     return store.dispatch(fetchUser()).then(() => {
       const actions = store.getActions();
-      expect(actions[0]).toEqual({ type: FETCH_USER });
+      expect(actions[0]).toEqual({ type: FETCH_USER, resData: fetchUserMock });
     });
   });
 });
-
-// import configureStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
-// import { logout, fetchUser } from '../../actions/userActions';
-// import { LOGOUT, FETCH_USER } from '../../actions/types';
-
-// process.on('unhandledRejection', (reason, p) => {
-//   console.log('Unhandled Rejection at:', p, 'reason:', reason);
-// });
-
-// console.log('actions.logout: ', logout);
-// const middlewares = [thunk];
-// const mockStore = configureStore(middlewares);
-
-// describe('User actions', () => {
-//   it('should handle LOGOUT action', () => {
-//     const store = mockStore({});
-
-//     console.log('actions.logout() ', logout());
-//     console.log('store ', store);
-
-//     console.log('store.dispatch(actions.logout()) ', store.dispatch(logout()));
-
-//     return store.dispatch(logout()).then(() => {
-//       const actions = store.getActions();
-//       console.log(store.getActions());
-//       expect(actions[0]).toEqual({ type: LOGOUT });
-//     });
-//   });
-
-//   it('should handle FETCH_USER action', () => {
-//     const store = mockStore({});
-
-//     console.log(
-//       'store.dispatch(actions.fetchUser()) ',
-//       store.dispatch(fetchUser())
-//     );
-
-//     return store.dispatch(fetchUser()).then(() => {
-//       const actions = store.getActions();
-//       console.log(store.getActions());
-//       expect(actions[0]).toEqual({ type: FETCH_USER });
-//     });
-//   });
-// });
