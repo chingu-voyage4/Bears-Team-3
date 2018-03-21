55cd475011c2a7d02a38ceee1cc5bc8bce8eb219
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-21 11:01:07 -0700

    Test handleMenu

--- client/src/tests/__tests__/Header.test.js
+++ client/src/tests/__tests__/Header.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
-//import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 import { Header } from './../../containers/Header';
@@ -23,10 +22,9 @@ describe('Header', () => {
       menuButton: test,
       flex: test,
     };
-    const wrapper = shallow(<Header classes={classes} />);
-    const IconButton = wrapper.find('IconButton').at(1);
-    console.log(IconButton);
-    IconButton.simulate('click', {
+    const wrapper = shallow(<Header classes={classes} auth={true} />);
+    const button = wrapper.find('IconButton').at(1);
+    button.simulate('click', {
       target: {
         value:
           '<button tabindex="0" class="MuiButtonBase-root-47 MuiIconButton-root-41 MuiIconButton-colorInherit-42" type="button" id="avatar" aria-haspopup="true" aria-owns="menu-appbar"><span class="MuiIconButton-label-46"><div class="MuiAvatar-root-87"><img alt="User Avatar" src="https://avatars2.githubusercontent.com/u/14152877?v=4" class="MuiAvatar-img-89"></div></span><span class="MuiTouchRipple-root-56"></span></button>',
