ea7c53c483eb985fe26e0548f5876b4485d2b9a2
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-27 11:57:49 -0700

    Remove unhandled promise rejection from tests

--- client/src/tests/__tests__/Home.test.js
+++ client/src/tests/__tests__/Home.test.js
@@ -4,6 +4,10 @@ import { Home } from './../../containers';
 import MockProvider from '../mockProvider';
 
 it('renders without crashing', () => {
-  const snap = shallow(<MockProvider><Home /></MockProvider>);
+  const snap = shallow(
+    <MockProvider>
+      <Home />
+    </MockProvider>
+  );
   expect(snap).toMatchSnapshot();
 });
