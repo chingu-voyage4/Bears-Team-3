44939d5a637f3575d5b9e50fc11c6dd3f554a727
Aaron <aaronmassey45@gmail.com>
2018-03-27 23:57:44 -0500

    Set total points min value at 0. Refactored route for new activity. Added route to delete an activity and update points.

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
 	githubId: { required: true, trim: true, type: String },
 	userName: { required: true, trim: true, type: String },
 	avatarURL: { required: true, trim: true, type: String },
-	totalPoints: { type: Number, required: true, default: 0 },
+	totalPoints: { type: Number, required: true, default: 0, min: 0 },
 });
 
 mongoose.model('users', userSchema);
