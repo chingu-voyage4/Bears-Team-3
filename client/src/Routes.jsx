e34caa3fab109c16648d4f34cdd59f965d87cbcd
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-03-30 05:44:46 -0700

    Add user route & component

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import { Home, About } from './containers';
+import { Home, About, User } from './containers';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/:userName" component={User} />
   </Switch>
 );
 
--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from './Home';
 export { default as About } from './About';
 export { default as Header } from './Header';
-//export { default as LeaderBoard } from './LeaderBoard';
+export { default as User } from './User';
