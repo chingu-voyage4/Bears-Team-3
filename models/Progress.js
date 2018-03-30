f4f27689eaf75ea1b265558e243cd3f4923b561b
Aaron <aaronmassey45@gmail.com>
2018-03-30 11:37:44 -0500

    Created progress/studyplan model

---
+++ models/Progress.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const requiredString = {
+	type: String,
+	required: true,
+};
+
+const ProgressSchema = new Schema({
+	goal: requiredString,
+	currentCourse: requiredString,
+	studyPlan: requiredString,
+	_user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+});
+
+mongoose.model('progress', ProgressSchema);
