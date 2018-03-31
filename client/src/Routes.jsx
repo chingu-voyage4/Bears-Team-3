d340a4de64b2801caff722f2f230abe7cae9e5e0
Aaron <aaronmassey45@gmail.com>
2018-03-30 20:26:41 -0500

    Created dummy Activities component to test actions

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import { Home, About } from './containers';
+import { Home, About, Activities } from './containers';
 
 const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-  </Switch>
+	<Switch>
+		<Route exact path="/" component={Home} />
+		<Route path="/about" component={About} />
+		<Route path="/activities" component={Activities} />
+	</Switch>
 );
 
 export default Routes;
