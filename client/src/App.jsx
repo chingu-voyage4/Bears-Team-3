31286524dd52dc507f9d4a9bbd8556d50a4a32a0
Aaron <aaronmassey45@gmail.com>
2018-03-30 20:26:08 -0500

    Added link to activities page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,49 +14,52 @@ import reactLogo from './assets/React-icon.png';
  * @return {Component} react base component
  */
 class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.getCurrentUser = this.getCurrentUser.bind(this);
-  }
+		this.getCurrentUser = this.getCurrentUser.bind(this);
+	}
 
-  componentDidMount() {
-    // Workaround to avoid re-rendering & CORS issues Credit @jenovs https://github.com/jenovs & https://stackoverflow.com/questions/28392393/passport-js-after-authentication-in-popup-window-close-it-and-redirect-the-pa/29314111#29314111
-    window.addEventListener('focus', this.getCurrentUser);
-    this.props.fetchUser();
-  }
+	componentDidMount() {
+		// Workaround to avoid re-rendering & CORS issues Credit @jenovs https://github.com/jenovs & https://stackoverflow.com/questions/28392393/passport-js-after-authentication-in-popup-window-close-it-and-redirect-the-pa/29314111#29314111
+		window.addEventListener('focus', this.getCurrentUser);
+		this.props.fetchUser();
+	}
 
-  getCurrentUser() {
-    this.props.fetchUser();
-  }
+	getCurrentUser() {
+		this.props.fetchUser();
+	}
 
-  render() {
-    return (
-      <BrowserRouter>
-        <main className="container">
-          <Header />
-          <div>
-            <h1>hello world!</h1>
-            <img
-              className="container__image"
-              alt="react logo"
-              src={reactLogo}
-            />
-            <p>If you see this everything is working!</p>
-          </div>
-          <ul className="left">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-          <Routes />
-        </main>
-      </BrowserRouter>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<main className="container">
+					<Header />
+					<div>
+						<h1>hello world!</h1>
+						<img
+							className="container__image"
+							alt="react logo"
+							src={reactLogo}
+						/>
+						<p>If you see this everything is working!</p>
+					</div>
+					<ul className="left">
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+						<li>
+							<Link to="/activities">Activities</Link>
+						</li>
+					</ul>
+					<Routes />
+				</main>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default connect(null, actions)(App);
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -5,22 +5,27 @@ import Button from 'material-ui/Button';
 
 import * as actions from '../actions';
 
-const Home = ({ counterValue, handleIncreaseValue, handleDecreaseValue }) => (
-  <div>
-    <h2>Home Page</h2>
-    <Button variant="raised" color="primary">
-      Hello World
-    </Button>
-    <p>The counter value is {counterValue}</p>
-    <button onClick={handleIncreaseValue}>Add</button>
-    <button onClick={handleDecreaseValue}>Remove</button>
-  </div>
+const Home = ({
+	counterValue,
+	totalPoints,
+	handleIncreaseValue,
+	handleDecreaseValue,
+}) => (
+	<div>
+		<h2>Home Page</h2>
+		<Button variant="raised" color="primary">
+			Hello World
+		</Button>
+		<p>The counter value is {counterValue}</p>
+		<button onClick={handleIncreaseValue}>Add</button>
+		<button onClick={handleDecreaseValue}>Remove</button>
+	</div>
 );
 
 Home.propTypes = {
-  counterValue: PropTypes.number.isRequired,
-  handleIncreaseValue: PropTypes.func.isRequired,
-  handleDecreaseValue: PropTypes.func.isRequired,
+	counterValue: PropTypes.number.isRequired,
+	handleIncreaseValue: PropTypes.func.isRequired,
+	handleDecreaseValue: PropTypes.func.isRequired,
 };
 
 // This is our redux wrapper component
@@ -28,16 +33,16 @@ Home.propTypes = {
 // and the 2 actions to increase and decrease counterValue by 1
 
 const mapStateToProps = state => ({
-  counterValue: state.counterReducer,
+	counterValue: state.counterReducer,
 });
 
 const mapDispatchToProps = dispatch => ({
-  handleIncreaseValue: () => {
-    dispatch(actions.addToCounter());
-  },
-  handleDecreaseValue: () => {
-    dispatch(actions.removeFromCounter());
-  },
+	handleIncreaseValue: () => {
+		dispatch(actions.addToCounter());
+	},
+	handleDecreaseValue: () => {
+		dispatch(actions.removeFromCounter());
+	},
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
