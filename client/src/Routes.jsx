b59506d1f001b8dcac124ca056544d5df499d22e
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-04 09:37:02 -0700

    Clean up routes

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import { Home, About, Activities } from './containers';
+import { Home, Activities } from './containers';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
     <Route exact path="/activities/:username" component={Activities} />
   </Switch>
 );
