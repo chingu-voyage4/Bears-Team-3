d487785b62b1c0754f05dd4a2a9823f13a9a6e37
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-12 10:02:54 -0700

    Get tabs example code working

--- client/src/containers/User.jsx
+++ client/src/containers/User.jsx
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import Typography from 'material-ui/Typography';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: theme.spacing.unit * 3,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
 
 class User extends Component {
   state = {
     user: null,
+    value: 0,
   };
 
   componentDidMount() {
     this.setState({ user: this.props.match.params.userName });
   }
 
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
-    const { user } = this.state;
+    const { classes } = this.props;
+    const { user, value } = this.state;
+
     return (
       <div>
         <h2>{user} Page</h2>
         <p>Welcome {user}!</p>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Tabs value={value} onChange={this.handleChange} centered>
+              <Tab label="Item One" />
+              <Tab label="Item Two" />
+              <Tab label="Item Three" href="#basic-tabs" />
+            </Tabs>
+          </AppBar>
+          {value === 0 && <TabContainer>Item One</TabContainer>}
+          {value === 1 && <TabContainer>Item Two</TabContainer>}
+          {value === 2 && <TabContainer>Item Three</TabContainer>}
+        </div>
       </div>
     );
   }
 }
 
-export default User;
+User.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(User);
