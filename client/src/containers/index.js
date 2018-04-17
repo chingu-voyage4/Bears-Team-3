06b19863901d0af4799a9a3ba5de86970ccc7db4
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-17 11:07:42 -0700

    Add DeleteUser to containers index

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as Header } from './Header';
 export { default as User } from './User';
 export { default as Activities } from './Activities';
+export { default as DeleteUser } from './DeleteUser';
