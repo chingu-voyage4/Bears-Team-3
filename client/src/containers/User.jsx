418f7cf5ed70fb2eed49bab7141a435f485c16ba
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-17 09:42:54 -0700

    Add check for activities

--- client/src/containers/User.jsx
+++ client/src/containers/User.jsx
@@ -102,7 +102,10 @@ class User extends Component {
             </AppBar>
             {value === 0 && (
               <TabContainer>
-                <UserActivities activities={this.props.activities} />
+                {console.log(this.props.activities)}
+                {this.props.activities && (
+                  <UserActivities activities={this.props.activities} />
+                )}
               </TabContainer>
             )}
             {value === 1 && (
