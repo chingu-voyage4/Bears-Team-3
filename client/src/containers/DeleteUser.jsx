ec2ef6f76cd97cad3f8e3dd0615b8531848a8f2d
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-17 11:55:43 -0700

    Delete user onClick

--- client/src/containers/DeleteUser.jsx
+++ client/src/containers/DeleteUser.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Button from 'material-ui/Button';
 
 import { deleteUser } from '../actions';
 
-const DeleteUser = () => (
-  <div>
-    <h2>Delete Your Account</h2>
-    <p>This will permanently remove all of your data</p>
-    <p>Are you sure?</p>
-    <Button variant="raised" color="primary">
-      Yes, Delete My Account
-    </Button>
-  </div>
-);
+class DeleteUser extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Delete Your Account</h2>
+        <p>This will permanently remove all of your data</p>
+        <p>Are you sure?</p>
+        <Button
+          variant="raised"
+          color="primary"
+          onClick={() => {
+            this.props.deleteUser();
+          }}
+        >
+          Yes, Delete My Account
+        </Button>
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({ deleteUser }, dispatch);
 };
 
-export default connect(mapDispatchToProps)(DeleteUser);
+export default connect(null, mapDispatchToProps)(DeleteUser);
