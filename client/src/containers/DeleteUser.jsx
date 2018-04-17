13d278300ae29034f30f62cd8ad21bd9aba58380
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-17 11:24:16 -0700

    Add copy to DeleteUser

--- client/src/containers/DeleteUser.jsx
+++ client/src/containers/DeleteUser.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Button from 'material-ui/Button';
+
+import { deleteUser } from '../actions';
 
 const DeleteUser = () => (
   <div>
-    <h2>Delete User Page</h2>
+    <h2>Delete Your Account</h2>
+    <p>This will permanently remove all of your data</p>
+    <p>Are you sure?</p>
+    <Button variant="raised" color="primary">
+      Yes, Delete My Account
+    </Button>
   </div>
 );
 
-export default DeleteUser;
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ deleteUser }, dispatch);
+};
+
+export default connect(mapDispatchToProps)(DeleteUser);
