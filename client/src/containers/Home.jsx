72763ddbb5e43cd4a67985729a70f1a0d70d206d
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-18 09:49:34 -0700

    Add landing page copy

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,11 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+
 import LeaderBoard from './LeaderBoard';
 
-const Home = () => (
-  <div>
-    <h2>Home Page</h2>
-    <LeaderBoard />
-  </div>
-);
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    width: '80%',
+    [theme.breakpoints.up('sm')]: {
+      width: '50%',
+    },
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+  }),
+});
 
-export default Home;
+const Home = props => {
+  const { classes } = props;
+  return (
+    <div>
+      <div className="home__intro">
+        <Paper className={classes.root} elevation={4}>
+          <Typography variant="headline" component="h3">
+            Welcome!
+          </Typography>
+          <Typography component="p">
+            Doing p1xt's{' '}
+            <a target="_blank" href="https://github.com/P1xt/speedstudy">
+              speedstudy
+            </a>?
+          </Typography>
+          <Typography component="p">
+            Log in for an easy way to track your progress!
+          </Typography>
+        </Paper>
+      </div>
+      <div className="home__intro">
+        <Paper className={classes.root} elevation={4}>
+          <Typography variant="headline" component="h3">
+            Participants
+          </Typography>
+        </Paper>
+      </div>
+
+      <LeaderBoard />
+    </div>
+  );
+};
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
