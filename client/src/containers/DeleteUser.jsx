8cf63978f8a9adb16e7cf487f9f2d09209829991
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-19 10:03:42 -0700

    Delete user not called

--- client/src/containers/DeleteUser.jsx
+++ client/src/containers/DeleteUser.jsx
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Redirect } from 'react-router-dom';
 import Button from 'material-ui/Button';
 
 import { deleteUser } from '../actions';
 
 class DeleteUser extends Component {
+  state = {
+    redirect: false,
+  };
+  handleClick = async () => {
+    console.log('handleClick called');
+    try {
+      let res = await this.props.deleteUser();
+      console.log(res);
+      if (res.payload.status === 400) throw new Error();
+      this.setState({ redirect: true });
+    } catch (err) {
+      return null;
+    }
+  };
   render() {
+    const { redirect } = this.state;
+    if (redirect) return <Redirect to="/" />;
     return (
       <div>
         <h2>Delete Your Account</h2>
         <p>This will permanently remove all of your data</p>
         <p>Are you sure?</p>
-        <Button
-          variant="raised"
-          color="primary"
-          onClick={() => {
-            this.props.deleteUser();
-          }}
-        >
+        <Button variant="raised" color="primary" onClick={this.handleClick}>
           Yes, Delete My Account
         </Button>
       </div>
