0f146db271dd8fd9cfc00d26341faa8d8d102a03
Aaron <aaronmassey45@gmail.com>
2018-04-19 12:19:52 -0500

    Added route to add new activity

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { Home, Activities, User } from './containers';
+import ActivityForm from './containers/activities/ActivityForm';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/users/:userName" component={User} />
     <Route exact path="/activities/:username" component={Activities} />
+    <Route path="/activity/new" component={ActivityForm} />
   </Switch>
 );
 
