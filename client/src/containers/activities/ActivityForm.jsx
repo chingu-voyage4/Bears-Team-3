acb7e41a3acdb9f7ab5bdffd6fcdd68430671372
Aaron <aaronmassey45@gmail.com>
2018-04-22 11:23:19 -0500

    Updated form for errors checking

--- client/src/containers/activities/ActivityForm.jsx
+++ client/src/containers/activities/ActivityForm.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { reduxForm, Field } from 'redux-form';
+import { withRouter } from 'react-router-dom';
 
 import ActivityInputField from './ActivityInputField';
+import ActivitySelectField from './ActivitySelectField';
 import { addActivity } from '../../actions';
 
 const FIELDS = [
   { label: 'Title', name: 'title' },
-  { label: 'URL', name: 'url' },
+  { label: 'Repo Link', name: 'url' },
   { label: 'Activity', name: 'activity' },
 ];
 
@@ -20,8 +22,9 @@ class ActivityForm extends Component {
           key={name}
           label={label}
           name={name}
-          component={ActivityInputField}
-          type="text"
+          component={
+            name === 'activity' ? ActivitySelectField : ActivityInputField
+          }
         />
       );
     });
@@ -31,7 +34,7 @@ class ActivityForm extends Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(values =>
-          this.props.addActivity(values)
+          this.props.addActivity(values, this.props.history)
         )}
       >
         {this.renderFields()}
@@ -45,8 +48,21 @@ const mapDispatchToProps = dispatch => {
   return bindActionCreators({ addActivity }, dispatch);
 };
 
-const connectedActivityForm = connect(null, mapDispatchToProps)(ActivityForm);
+const connectedActivityForm = connect(null, mapDispatchToProps)(
+  withRouter(ActivityForm)
+);
+
+const validate = values => {
+  const errors = {};
+
+  FIELDS.forEach(({ name }) => {
+    if (!values[name]) errors[name] = `You must have a ${name}`;
+  });
+
+  return errors;
+};
 
 export default reduxForm({
+  validate,
   form: 'activityForm',
 })(connectedActivityForm);
