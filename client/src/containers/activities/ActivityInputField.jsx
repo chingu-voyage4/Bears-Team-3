87706e1d8f96568b7468aef34e16ab6cce160496
Aaron <aaronmassey45@gmail.com>
2018-04-23 11:02:49 -0500

    Added margin

--- client/src/containers/activities/ActivityInputField.jsx
+++ client/src/containers/activities/ActivityInputField.jsx
@@ -7,8 +7,7 @@ const styles = theme => ({
     flexWrap: 'wrap',
   },
   textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
+    margin: theme.spacing.unit,
     width: 200,
   },
   menu: {
