85f142313cdbde4dbe5062f99204c42205fe3e09
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-29 16:58:17 -0700

    Remove console.log

--- client/src/containers/UserActivities.jsx
+++ client/src/containers/UserActivities.jsx
@@ -36,7 +36,6 @@ const styles = theme => ({
 export class UserActivities extends Component {
   handleDelete = id => {
     this.props.deleteActivity(id);
-    console.log(this.props.userPage._user);
     this.props.fetchActivities(this.props.userPage._user);
   };
 
