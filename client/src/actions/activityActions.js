2809a0216971e543a4479b8cebd388de8ecae402
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-29 16:48:19 -0700

    Delete console.logs from activityActions

--- client/src/actions/activityActions.js
+++ client/src/actions/activityActions.js
@@ -9,13 +9,11 @@ export const addActivity = (values, history, userName) => async dispatch => {
 
 export const fetchActivities = id => async dispatch => {
   const res = await axios.get(`/api/activities/${id}`);
-  console.log(res.data);
   dispatch({ type: FETCH_ACTIVITIES, payload: res.data });
 };
 
 export const deleteActivity = id => async dispatch => {
   const res = await axios.delete(`/api/activity/${id}`);
-  console.log(res.data);
   dispatch({ type: FETCH_USER_PAGE, payload: res.data });
 };
 
