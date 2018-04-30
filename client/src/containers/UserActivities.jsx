13c2f69e10e1045d4ae11bef0fdb7b8951ad42d7
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-04-29 17:09:08 -0700

    Call fetchActivities with _id

--- client/src/containers/UserActivities.jsx
+++ client/src/containers/UserActivities.jsx
@@ -36,7 +36,7 @@ const styles = theme => ({
 export class UserActivities extends Component {
   handleDelete = id => {
     this.props.deleteActivity(id);
-    this.props.fetchActivities(this.props.userPage._user);
+    this.props.fetchActivities(this.props.userPage._id);
   };
 
   render() {
