a4920635e810b5e80637670a3d5763cf734f5e63
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-01 13:52:19 -0700

    Start ProgressEdit

---
+++ client/src/containers/progress/ProgressEdit.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+
+import ProgressPageOne from './ProgressPageOne';
+import ProgressPageTwo from './ProgressPageTwo';
+import ProgressPageThree from './ProgressPageThree';
+
+class ProgressEdit extends Component {
+  state = { showPage: 1 };
+
+  showPage = value => {
+    if (value > 0 && value < 3) {
+      this.setState({ showPage: value });
+    }
+  };
+
+  renderContent = () => {
+    const { showPage } = this.state;
+    const { user } = this.props;
+
+    if (showPage === 1) {
+      return <ProgressPageOne showPage={this.showPage} user={user} />;
+    } else if (showPage === 2) {
+      return <ProgressPageTwo showPage={this.showPage} />;
+    } else {
+      return <ProgressPageThree showPage={this.showPage} user={user} />;
+    }
+  };
+
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}
+
+const mapStateToProps = state => ({
+  user: state.authReducer.userName,
+});
+
+export default reduxForm({ form: 'progressForm' })(
+  connect(mapStateToProps)(ProgressEdit)
+);
