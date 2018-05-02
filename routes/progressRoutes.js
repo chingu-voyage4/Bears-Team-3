3faf463df8aa54e797b2b8e1f752c5cac4a2d3f3
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-02 08:57:08 -0700

    Remove error if no progress data found

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -14,9 +14,9 @@ module.exports = app => {
       if (!isValidId(userId)) throw new Error('Invalid id');
 
       const progress = await Progress.findOne({ _user: userId });
-      if (!progress) throw new Error('No progress data found');
+      //if (!progress) throw new Error('No progress data found');
 
-      res.send(progress);
+      res.send(progress || 'No progress data found');
     } catch (err) {
       res.status(400).send(err.message);
     }
