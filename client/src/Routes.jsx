dbf00a9d5db15dbbc6c705a9bffa4fe428753df0
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-03 12:07:23 -0700

    Remove Activities route

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import { Home, Activities, User, DeleteUser } from './containers';
+import { Home, User, DeleteUser } from './containers';
 import ActivityForm from './containers/activities/ActivityForm';
 import ProgressNew from './containers/progress/ProgressNew';
 import ProgressEdit from './containers/progress/ProgressEdit';
@@ -10,7 +10,6 @@ const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/users/:userName" component={User} />
-    <Route exact path="/activities/:username" component={Activities} />
     <Route path="/delete" component={DeleteUser} />
     <Route path="/activity/new" component={ActivityForm} />
     <Route path="/activity/edit" component={ActivityForm} />
