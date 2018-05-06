293a4b3a35fc8e67f7a0ae0195b1ace68d4eb7e8
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-06 10:05:11 -0700

    Americanize dates in datepicker if language is en-US

--- client/src/containers/activities/ActivityDatePicker.jsx
+++ client/src/containers/activities/ActivityDatePicker.jsx
@@ -16,6 +16,11 @@ class DateSelector extends Component {
     });
   };
 
+  componentDidMount() {
+    const language = window.navigator.language;
+    this.setState({ lang: language });
+  }
+
   render() {
     const { classes, input, label } = this.props;
     return (
@@ -26,7 +31,7 @@ class DateSelector extends Component {
             animateYearScrolling={false}
             className={classes.textField}
             disableFuture
-            format="DD MMM YYYY"
+            format={this.state.lang === 'en-US' ? 'MMM DD YYYY' : 'DD MMM YYYY'}
             label={label}
             maxDateMessage="You can't add future accomplishments!"
             minDate="2018-01-01"
